6f330b5244114732127630d146ccddeffaaa2114
0xb10c <b10c@b10c.me>
2026-04-02 19:25:35 +0200

    use: dev branch to test PRs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     infra-library = {
-      url = "github:peer-observer/infra-library";
+      url = "github:peer-observer/infra-library?ref=2026-04-peer-observer-dev";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
